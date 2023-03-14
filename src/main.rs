ca0cf41da0f5dac4884cb5ddccaa21b19850b356
Robert812218 <robert812@shaw.ca>
2023-03-14 10:29:44 -0600

    fix server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::io::{Error, ErrorKind};
 use std::str::FromStr;
+use std::io::{Error, ErrorKind};
+
+use warp::Filter;
 
 #[derive(Debug)]
 struct Question {
@@ -13,41 +15,15 @@ struct Question {
 struct QuestionId(String);
 
 impl Question {
-    fn new(
-        id: QuestionId,
-        title: String,
-        content: String,
-        tags: Option<Vec<String>>,
-    ) -> Self {
-        id,
-        title,
-        content,
-        tags,
-    }
-
-    fn fmt(&self, f:&mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-        write!(
-            f,
-            "{}, title: {}, content: {}, tags: {:?",
-            self.id, self.title, self.content, self.tags
-        )
-    }
-}
-
-impl std::fmt::Display for Question {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
-        write!(f, "id: {:?}", self.tags)
-    }
-}
-
-assert_eq!("(1.987, 2.983)",
-    format!(
-        "{}",
-        Position {
-            longitude: 1.987, latitude: 2.983,
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
         }
-    )
-);
+    }
+}
 
 impl FromStr for QuestionId {
     type Err = std::io::Error;
@@ -55,14 +31,13 @@ impl FromStr for QuestionId {
     fn from_str(id: &str) -> Result<Self, Self::Err> {
         match id.is_empty() {
             false => Ok(QuestionId(id.to_string())),
-            true => Err(
-                Error::new(ErrorKind::InvalidInput, "No id provided")
-            ),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
         }
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let hello = warp::get()
         .map(|| format!("Hello, World!"));
 
