cadfa8ae9613a85dbe0541ffd8a421d2f064abc3
Robert812218 <robert812@shaw.ca>
2023-03-25 14:09:59 -0600

    update get_questions to integrate to postgres

--- src/store.rs
+++ src/store.rs
@@ -1,8 +1,8 @@
 use sqlx::postgres::{PgPoolOptions, PgPool, PgRow};
 use sqlx::Row;
 
+use handle_errors::Error;
 use crate::types::{
-    answer::{Answer, AnswerId},
     question::{Question, QuestionId},
 };
 
@@ -17,11 +17,35 @@ impl Store {
             .max_connections(5)
             .connect(db_url).await {
                 Ok(pool) => pool,
-                Err(e) => panic!("Couldn't establish a DB connection: {}", e),
-            };
+                Err(_) => panic!("Couldn't establish DB connection"),
+        };
 
         Store {
             connection: db_pool,
-        }
+        } 
+    }
+
+    pub async fn get_questions(
+        &self,
+        limit: Option<u32>,
+        offset: u32
+    ) -> Result<Vec<Question>, sqlx::Error> {
+        match sqlx::query("SELECT * from questions LIMIT $1 OFFSET $2")
+            .bind(limit)
+            .bind(offset)
+            .map(|row: PgRow| Question {
+                id: QuestionId(row.get("id")),
+                title: row.get("title"),
+                content: row.get("content"),
+                tags: row.get("tags"),
+            })
+            .fetch_all(&self.connection)
+            .await {
+                Ok(questions) => Ok(questions),
+                Err(e) => {
+                    tracing::event!(tracing::Level::ERROR, "{:?}", e);
+                    Err(Error::DatabaseQueryError)
+                }
+            }
     }
 }
