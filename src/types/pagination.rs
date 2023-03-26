964a746bc4c62587ccfd58330f5e05ec48703c2f
Robert812218 <robert812@shaw.ca>
2023-03-26 10:05:32 -0600

    update question.rs with the Default trait and renaming

--- src/types/pagination.rs
+++ src/types/pagination.rs
@@ -7,9 +7,9 @@ use handle_errors::Error;
 #[derive(Debug)]
 pub struct Pagination {
     /// The index of the first item which has to be returned
-    pub start: usize,
+    pub limit: Option<u32>,
     /// The index of the last item which has to be returned
-    pub end: usize,
+    pub offset: u32,
 }
 /// Extract query parameters from the `/questions` route
 /// # Example query
@@ -31,17 +31,17 @@ pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination,
     // Could be improved in the future
     if params.contains_key("start") && params.contains_key("end") {
         return Ok(Pagination {
-            // Takes the "start" parameter in the query and tries to convert it to a number
-            start: params
-                .get("start")
+            limit: Some(params
+                .get("limit")
                 .unwrap()
-                .parse::<usize>()
-                .map_err(Error::ParseError)?,
-            // Takes the "end" parameter in the query and tries to convert it to a number
-            end: params
-                .get("end")
+                .parse::<u32>()
+                .map_err(Error::ParseError()?),
+            // Takes the "offset" parameter in the query
+            // and tries to convert it into a number
+            offset: params
+                .get("offset")
                 .unwrap()
-                .parse::<usize>()
+                .parse::<u32>()
                 .map_err(Error::ParseError)?,
         });
     }
